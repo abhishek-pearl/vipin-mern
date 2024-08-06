b0cd959366e329dd47b46d81dd15104163364ce0
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-08-06 15:37:55 +0530

    worked on .env

--- src/components/GetLoanModal/GetLoanForm.js
+++ src/components/GetLoanModal/GetLoanForm.js
@@ -5,199 +5,193 @@ import React, { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { IoMdClose } from "react-icons/io";
 import { toast } from "sonner";
+import { useForm } from "react-hook-form";
 
 const GetLoanForm = ({ setLoanModal, loanModal }) => {
   const [mounted, setMounted] = useState(false);
   const [loading, setLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     setMounted(true);
-
     return () => setMounted(false);
   }, []);
-  async function submitLoan(formData) {
-    const name = formData.get("name");
-    const email = formData.get("email");
-    const mobile = formData.get("mobile");
-    const typeOfLoan = formData.get("typeOfLoan");
-    const loanRequired = formData.get("loanRequired");
-    const pincode = formData.get("pincode");
+
+  const submitLoan = async (data) => {
     try {
       setLoading(true);
       const response = await axios.post(
-        `${process.env.API_URL}/inquiry`,
-        { name, email, mobile, typeOfLoan, loanRequired, pincode }
+        `${process.env.NEXT_PUBLIC_API_URL}/inquiry`,
+        data
       );
       setLoading(false);
-      toast.success("Successfully Submited...");
-
+      toast.success("Successfully Submitted...");
       console.log(response, "response");
     } catch (error) {
       setLoading(false);
-      toast.success("Event has been created");
+      toast.error("Server is busy. Please try again later.");
       console.log(error.message);
     }
+  };
 
-    console.log(name, email, mobile, typeOfLoan, loanRequired);
-  }
-  return (
-    <>
-      {mounted &&
-        loanModal &&
-        createPortal(
-          <div className="  bg-black/30  fixed top-0 left-0  h-full w-full ">
-            <button
-              onClick={() => {
-                setLoanModal(!loanModal);
-              }}
-              className="absolute top-3 right-3 z-50 text-black"
+  if (!mounted || !loanModal) return null;
+
+  return createPortal(
+    <div className="bg-black/30 fixed top-0 left-0 h-full w-full flex items-center justify-center z-50">
+      <div className="bg-white w-full max-w-lg mx-4 md:mx-auto p-6 rounded-lg shadow-lg relative">
+        <button
+          onClick={() => setLoanModal(false)}
+          className="absolute top-3 right-3 text-black"
+        >
+          <IoMdClose size={30} />
+        </button>
+        <div className="text-center mb-8">
+          <img
+            src="https://floatui.com/logo.svg"
+            width={150}
+            alt="Logo"
+            className="mx-auto"
+          />
+          <h3 className="text-2xl font-bold text-gray-800 mt-5">
+            Enquiry Form
+          </h3>
+        </div>
+        <form onSubmit={handleSubmit(submitLoan)} className="space-y-5">
+          <div>
+            <label className="font-medium">Name</label>
+            <input
+              type="text"
+              {...register("name", { required: "Name is required" })}
+              className={`w-full mt-2 px-3 py-2 border rounded-lg shadow-sm focus:border-indigo-600 ${
+                errors.name ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+            {errors.name && (
+              <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+            )}
+          </div>
+          <div>
+            <label className="font-medium">Email</label>
+            <input
+              type="email"
+              {...register("email", { required: "Email is required" })}
+              className={`w-full mt-2 px-3 py-2 border rounded-lg shadow-sm focus:border-indigo-600 ${
+                errors.email ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label className="font-medium">Mobile No.</label>
+            <input
+              type="tel"
+              {...register("mobile", { required: "Mobile number is required" })}
+              className={`w-full mt-2 px-3 py-2 border rounded-lg shadow-sm focus:border-indigo-600 ${
+                errors.mobile ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+            {errors.mobile && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.mobile.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label className="font-medium">Pincode</label>
+            <input
+              type="text"
+              {...register("pincode", { required: "Pincode is required" })}
+              className={`w-full mt-2 px-3 py-2 border rounded-lg shadow-sm focus:border-indigo-600 ${
+                errors.pincode ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+            {errors.pincode && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.pincode.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label htmlFor="loanType" className="font-medium">
+              Loan Type
+            </label>
+            <select
+              id="loanType"
+              {...register("typeOfLoan", {
+                required: "Please select a loan type",
+              })}
+              className={`w-full mt-2 px-3 py-2 border rounded-lg shadow-sm focus:border-indigo-600 ${
+                errors.typeOfLoan ? "border-red-500" : "border-gray-300"
+              }`}
             >
-              <IoMdClose size={30} />
-            </button>
-
-            <div className="w-full grid md:grid-cols-[auto_35rem] h-full">
-              <div className="hidden md:block"></div>
-              <div
-                className={`  flex items-center justify-center h-full  bg-white ${
-                  loanModal ? "translate-x-0" : "translate-x-[100%]"
-                } transition-all`}
-              >
-                <div className="w-full max-w-md space-y-8 px-4 bg-white text-gray-600 sm:px-0">
-                  <div className="">
-                    <img
-                      src="https://floatui.com/logo.svg"
-                      width={150}
-                      className="lg:hidden"
-                    />
-                    <div className="mt-5 space-y-2">
-                      <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">
-                        Enquiry Form
-                      </h3>
-                    </div>
-                  </div>
-
-                  <form action={submitLoan} className="space-y-5">
-                    <div>
-                      <label className="font-medium">Name</label>
-                      <input
-                        type="text"
-                        name="name"
-                        required
-                        className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                      />
-                    </div>
-                    <div>
-                      <label className="font-medium">Email</label>
-                      <input
-                        type="email"
-                        name="email"
-                        required
-                        className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                      />
-                    </div>
-                    <div>
-                      <label className="font-medium">Mobile No.</label>
-                      <input
-                        name="mobile"
-                        type="mobile"
-                        required
-                        className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                      />
-                    </div>
-                    <div>
-                      <label className="font-medium">Pincode</label>
-                      <input
-                        name="pincode"
-                        type="pincode"
-                        required
-                        className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                      />
-                    </div>
-                    <div>
-                      <label htmlFor="loanType" className="font-medium">
-                        Loan Type
-                      </label>
-                      <select
-                        id="loanType"
-                        name="typeOfLoan"
-                        required
-                        className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                      >
-                        <option value="">Select a loan type</option>
-                        <option value="personal">Personal Loan</option>
-                        <option value="auto">Auto Loan</option>
-                        <option value="home">Home Loan</option>
-                        <option value="student">Student Loan</option>
-                        <option value="business">Business Loan</option>
-                      </select>
-                    </div>
-                    <div>
-                      <p class="font-medium mb-1 text-gray-500">
-                        Loan Required?
-                      </p>
-                      <div class="flex gap-x-4">
-                        <div class="relative flex w-56 items-center justify-center rounded-xl bg-gray-50 px-4 py-3 font-medium text-gray-700">
-                          <input
-                            class="peer hidden"
-                            type="radio"
-                            name="loanRequired"
-                            value="yes"
-                            id="radio1"
-                            checked
-                          />
-                          <label
-                            class="peer-checked:border-red-400 peer-checked:bg-red-200 absolute top-0 h-full w-full cursor-pointer rounded-xl border"
-                            for="radio1"
-                          >
-                            {" "}
-                          </label>
-                          <div class="peer-checked:border-transparent peer-checked:bg-red-400 peer-checked:ring-2 absolute left-4 h-5 w-5 rounded-full border-2 border-gray-300 bg-gray-200 ring-red-400 ring-offset-2"></div>
-                          <span class="pointer-events-none z-10">Yes</span>
-                        </div>
-                        <div class="relative flex w-56 items-center justify-center rounded-xl bg-gray-50 px-4 py-3 font-medium text-gray-700">
-                          <input
-                            class="peer hidden"
-                            type="radio"
-                            name="loanRequired"
-                            value="no"
-                            id="radio3"
-                            checked
-                          />
-                          <label
-                            class="peer-checked:border-red-400 peer-checked:bg-red-200 absolute top-0 h-full w-full cursor-pointer rounded-xl border"
-                            for="radio3"
-                          >
-                            {" "}
-                          </label>
-                          <div class="peer-checked:border-transparent peer-checked:bg-red-400 peer-checked:ring-2 absolute left-4 h-5 w-5 rounded-full border-2 border-gray-300 bg-gray-200 ring-red-400 ring-offset-2"></div>
-                          <span class="pointer-events-none z-10">No</span>
-                        </div>
-                      </div>
-                    </div>
-
-                    {loading ? (
-                      <button
-                        type="button"
-                        className="w-full px-4 py-2 text-white font-medium bg-red-600 hover:bg-red-500 active:bg-red-600 rounded-lg duration-150"
-                      >
-                        Loading...
-                      </button>
-                    ) : (
-                      <button
-                        type="submit"
-                        className="w-full px-4 py-2 text-white font-medium bg-red-600 hover:bg-red-500 active:bg-red-600 rounded-lg duration-150"
-                      >
-                        Submit
-                      </button>
-                    )}
-                  </form>
-                </div>
-              </div>
+              <option value="">Select a loan type</option>
+              <option value="personal">Personal Loan</option>
+              <option value="auto">Auto Loan</option>
+              <option value="home">Home Loan</option>
+              <option value="student">Student Loan</option>
+              <option value="business">Business Loan</option>
+            </select>
+            {errors.typeOfLoan && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.typeOfLoan.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <p className="font-medium mb-1 text-gray-500">Loan Required?</p>
+            <div className="flex gap-x-4">
+              <label className="relative flex items-center justify-center w-1/2 bg-gray-50 px-4 py-3 font-medium text-gray-700 rounded-xl">
+                <input
+                  type="radio"
+                  {...register("loanRequired", {
+                    required: "Please select if loan is required",
+                  })}
+                  value="yes"
+                  className="peer hidden"
+                />
+                <div className="peer-checked:border-transparent peer-checked:bg-red-400 peer-checked:ring-2 absolute left-4 h-5 w-5 rounded-full border-2 border-gray-300 bg-gray-200 ring-red-400 ring-offset-2"></div>
+                <span className="pointer-events-none z-10">Yes</span>
+              </label>
+              <label className="relative flex items-center justify-center w-1/2 bg-gray-50 px-4 py-3 font-medium text-gray-700 rounded-xl">
+                <input
+                  type="radio"
+                  {...register("loanRequired", {
+                    required: "Please select if loan is required",
+                  })}
+                  value="no"
+                  className="peer hidden"
+                />
+                <div className="peer-checked:border-transparent peer-checked:bg-red-400 peer-checked:ring-2 absolute left-4 h-5 w-5 rounded-full border-2 border-gray-300 bg-gray-200 ring-red-400 ring-offset-2"></div>
+                <span className="pointer-events-none z-10">No</span>
+              </label>
             </div>
-          </div>,
-          document.body
-        )}
-    </>
+            {errors.loanRequired && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.loanRequired.message}
+              </p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className={`w-full px-4 py-2 text-white font-medium bg-red-600 hover:bg-red-500 active:bg-red-600 rounded-lg duration-150 ${
+              loading ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "Submit"}
+          </button>
+        </form>
+      </div>
+    </div>,
+    document.body
   );
 };
 
